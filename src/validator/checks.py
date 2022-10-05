83253fae5c8c31e419a6a0e5fa7859a68e9d043c
Jake Adams <jdadams@utah.gov>
2022-10-05 16:30:53 -0600

    chore: initial checks, reading

---
+++ src/validator/checks.py
@@ -0,0 +1,190 @@
+import re
+
+import pandas as pd
+
+
+def column_name_check(dataframe):
+    good_column_names = {
+        'contact_name',
+        'contact_email',
+        'contact_phone',
+        'category_code',
+        'location_id',
+        'address_primary',
+        'city',
+        'state',
+        'zip_code',
+        'zip_code_suffix',
+        'unit_count',
+        'building_type_code',
+        'non_bsl_code',
+        'bsl_lacks_address_flag',
+        'latitude',
+        'longitude',
+    }
+
+    dataframe_column_names = set(dataframe.columns)
+
+    matched_names = good_column_names & dataframe_column_names
+    missing_good_names = good_column_names - dataframe_column_names
+    extra_dataframe_names = dataframe_column_names - good_column_names
+
+    return (matched_names, missing_good_names, extra_dataframe_names)
+
+
+def category_code_check(row):
+    cat_code = row['category_code']
+    if cat_code < 1 or cat_code > 7:
+        return 'Invalid category_code'
+    return 'Valid'
+
+
+def location_id_check(row):
+    #: need to make sure we clean up any blank/empty strings in 'location_id'first.
+    #: '<Null>'s may need to be cleaned up as well.
+    #: need to make sure this is loaded as a string
+
+    if row['location_id']:
+        if not re.search('[0-9]{10}', row['location_id']):
+            return 'location_id must be a 10-digit number'
+
+        if row['category_code'] == 1:
+            return 'location_id must be empty for category_code 1'
+
+    if not row['location_id'] and row['category_code'] != 1:
+        return 'location must be included for category_code 2-7'
+
+    return 'Valid'
+
+
+def address_primary_check(row):
+
+    if not row['address_primary']:
+        if row['category_code'] == 7:
+            return 'address_primary required for category_code 7'
+        if row['category_code'] in (1, 2) and row['bsl_lacks_address_flag'] == 0:
+            return 'address_primary required for category codes 1 or 2 with bsl_lacks_address_flag set to 0'
+
+    if row['address_primary']:
+        if row['category_code'] in (3, 4, 5, 6):
+            return 'address_primary must be empty for category_codes 3-6'
+        if row['category_code'] in (1, 2) and row['bsl_lacks_address_flag'] == 1:
+            return 'address_primary must be empty for category codes 1 or 2 with bsl_lacks_address_flag set to 1'
+
+    return 'Valid'
+
+
+def city_check(row):
+    if row['city'] and row['category_code'] in (3, 4, 5, 6):
+        return 'city must be empty for category codes 3-6'
+    if not row['city'] and row['category_code'] in (1, 2, 7):
+        return 'city required for category codes 1, 2, or 7'
+
+    return 'Valid'
+
+
+def state_check(row, state_code='UT'):
+
+    if row['state'] and row['category_code'] in (3, 4, 5, 6):
+        return 'state must be empty for category codes 3-6'
+    if not row['state'] and row['category_code'] in (1, 2, 7):
+        return 'state required for category codes 1, 2, or 7'
+    if row['state'] and row['state'] != state_code:
+        return f'state must be {state_code}'
+    return 'Valid'
+
+
+def zip_code_check(row, zip_prefix='84'):
+
+    if not row['zip_code']:
+        if row['category_code'] == 7:
+            return 'zip_code required for category_code 7'
+        if row['category_code'] in (1, 2) and row['bsl_lacks_address_flag'] == 0:
+            return 'zip_code required for category codes 1 or 2 with bsl_lacks_address_flag set to 0'
+
+    if row['zip_code'] and row['category_code'] in (3, 4, 5, 6):
+        return 'zip_code must be empty for category_codes 3-6'
+
+    pattern = zip_prefix + '[0-9]{3}'
+    if not re.match(pattern, row['zip_code']):
+        return f'zip_code must in the form {zip_prefix}xyz'
+
+    return 'Valid'
+
+
+def zip_code_suffix_check(row):
+    if row['zip_code_suffix'] and row['category_code'] in (3, 4, 5, 6):
+        return 'zip_code_suffix must be empty for category_codes 3-6'
+
+    if not re.match('^[0-9]{4}$', row['zip_code_suffix']) and row['category_code'] not in (3, 4, 5, 6):
+        return 'zip_code_suffix must be a four-digit number'
+
+    return 'Valid'
+
+
+def unit_count_check(row):
+    if not row['unit_count'] and row['category_code'] in (1, 3):
+        return 'unit_count required for category codes 1 and 3'
+    if row['unit_count'] and row['category_code'] in (2, 4, 5, 6, 7):
+        return 'unit_count must be empty for category codes 2, 4, 5, 6, 7'
+    return 'Valid'
+
+
+def building_type_code_check(row):
+    allowed_values = ('B', 'R', 'X', 'G', 'C', 'E')
+    if row['building_type_code'] and row['category_code'] in (2, 3, 5, 6, 7):
+        return 'building_type_code must be empty for category codes 2, 3, 5, 6, 7'
+    if row['category_code'] in (1, 4) and row['building_type_code'] not in allowed_values:
+        return 'building_type_code must be B, R, X, G, C, or E for category codes 1 and 4'
+    return 'Valid'
+
+
+def non_bsl_code_check(row):
+    allowed_values = ('S', 'L', 'G', 'H', 'F', 'E', 'P', 'N', 'R')
+    if row['non_bsl_code'] and row['category_code'] in (1, 2, 3, 4, 5, 7):
+        return 'non_bsl_code must be empty for category codes 1, 2, 3, 4, 5, 7'
+    if row['category_code'] == 6 and row['non_bsl_code'] not in allowed_values:
+        return 'non_bsl_code must be S, L, G, H, F, E, P, N, or R for category code 6'
+    return 'Valid'
+
+
+def bsl_lacks_address_flag_check(row):
+    #: convert field to str
+    if row['bsl_lacks_address_flag'] not in ('0', '1') and row['category_code'] in (1, 2):
+        return 'bsl_lacks_address_flag must have a value of 0 or 1 for category code 1 or 2'
+    if row['bsl_lacks_address_flag'] and row['category_code'] in (3, 4, 5, 6, 7):
+        return 'bsl_lacks_address_flag must be empty for category codes 3-7'
+    # if row['bsl_lacks_address_flag'] not in (0, 1):
+    #     return 'bsl_lacks_address_flag must have a value of 0 or 1'
+
+    return 'Valid'
+
+
+def latitude_check(row, min_lat=36.8, max_lat=42.2):
+    #: Needs to come in as str to maintain precision check (casting float to str will lose any trailing 0s)
+    if row['latitude'] and row['category_code'] in (2, 3, 4, 6, 7):
+        return 'latitude must be empty for category codes 2, 3, 4, 6, 7'
+    if not row['latitude'] and row['category_code'] in (1, 5):
+        return 'latitude required for category codes 1 and 5'
+
+    if row['latitude'] and row['category_code'] in (1, 5):
+        if not min_lat < float(row['latitude']) < max_lat:
+            return f'latitude out of range {min_lat} to {max_lat}'
+        if not re.search(r'(?<=\.)[0-9]{6,}', row['latitude']):
+            return 'latitude must have a minimum precision of 6 decimal places'
+    return 'Valid'
+
+
+def longitude_check(row, min_long=-114.3, max_long=-109):
+    #: Needs to come in as str to maintain precision check (casting float to str will lose any trailing 0s)
+    if row['longitude'] and row['category_code'] in (2, 3, 4, 6, 7):
+        return 'longitude must be empty for category codes 2, 3, 4, 6, 7'
+    if not row['longitude'] and row['category_code'] in (1, 5):
+        return 'longitude required for category codes 1 and 5'
+
+    if row['longitude'] and row['category_code'] in (1, 5):
+        if not min_long < float(row['longitude']) < max_long:
+            return f'longitude out of range {min_long} to {max_long}'
+        if not re.search(r'(?<=\.)[0-9]{6,}', row['longitude']):
+            return 'longitude must have a minimum precision of 6 decimal places'
+    return 'Valid'
---
+++ tests/test_checks.py
@@ -0,0 +1,459 @@
+import pandas as pd
+
+from validator import checks
+
+
+def test_category_codes():
+
+    rows = [{'category_code': foo} for foo in range(0, 9)]
+
+    output = []
+    for row in rows:
+        output.append(checks.category_code_check(row))
+
+    test_output = [
+        'Invalid category_code', 'Valid', 'Valid', 'Valid', 'Valid', 'Valid', 'Valid', 'Valid', 'Invalid category_code'
+    ]
+
+    assert output == test_output
+
+
+def test_location_id_check_format():
+    # yapf: disable
+    rows = [
+        {'location_id': '123456', 'category_code': -1},
+        {'location_id': 'string', 'category_code': -1},
+        {'location_id': '123withstring', 'category_code': -1},
+        {'location_id': '1234567890', 'category_code': -1}
+    ]
+    # yapf: enable
+
+    output = []
+    for row in rows:
+        output.append(checks.location_id_check(row))
+
+    test_output = [
+        'location_id must be a 10-digit number',
+        'location_id must be a 10-digit number',
+        'location_id must be a 10-digit number',
+        'Valid',
+    ]
+
+    assert output == test_output
+
+
+def test_location_id_check_category_code_checks():
+    # yapf: disable
+    rows = [
+        {'location_id': '1234567890', 'category_code': 1},
+        {'location_id': '1234567890', 'category_code': 2},
+        {'location_id': '', 'category_code': 1},
+        {'location_id': '', 'category_code': 2}
+    ]
+    # yapf: enable
+
+    output = []
+    for row in rows:
+        output.append(checks.location_id_check(row))
+
+    test_output = [
+        'location_id must be empty for category_code 1',
+        'Valid',
+        'Valid',
+        'location must be included for category_code 2-7',
+    ]
+
+    assert output == test_output
+
+
+def test_address_primary_check():
+    # yapf: disable
+    rows = [
+        {'address_primary': '', 'category_code': 7},
+        {'address_primary': '', 'category_code': 2, 'bsl_lacks_address_flag': 0},
+        {'address_primary': '', 'category_code': 2, 'bsl_lacks_address_flag': 1},
+        {'address_primary': '', 'category_code': 3},
+        {'address_primary': 'addr', 'category_code': 3},
+        {'address_primary': 'addr', 'category_code': 2, 'bsl_lacks_address_flag': 1},
+        {'address_primary': 'addr', 'category_code': 2, 'bsl_lacks_address_flag': 0},
+        {'address_primary': 'addr', 'category_code': 7},
+    ]
+    # yapf: enable
+
+    output = []
+    for row in rows:
+        output.append(checks.address_primary_check(row))
+
+    test_output = [
+        'address_primary required for category_code 7',
+        'address_primary required for category codes 1 or 2 with bsl_lacks_address_flag set to 0',
+        'Valid',
+        'Valid',
+        'address_primary must be empty for category_codes 3-6',
+        'address_primary must be empty for category codes 1 or 2 with bsl_lacks_address_flag set to 1',
+        'Valid',
+        'Valid',
+    ]
+
+    assert output == test_output
+
+
+def test_city_check():
+    # yapf: disable
+    rows = [
+        {'city': 'foo', 'category_code': 3},
+        {'city': 'foo', 'category_code': 1},
+        {'city': '', 'category_code': 1},
+        {'city': '', 'category_code': 3},
+
+    ]
+    # yapf: enable
+
+    output = []
+    for row in rows:
+        output.append(checks.city_check(row))
+
+    test_output = [
+        'city must be empty for category codes 3-6',
+        'Valid',
+        'city required for category codes 1, 2, or 7',
+        'Valid',
+    ]
+
+    assert output == test_output
+
+
+def test_state_check():
+    # yapf: disable
+    rows = [
+        {'state': 'UT', 'category_code': 3},
+        {'state': 'UT', 'category_code': 1},
+        {'state': 'ZZ', 'category_code': 1},
+        {'state': '', 'category_code': 1},
+        {'state': '', 'category_code': 3},
+
+    ]
+    # yapf: enable
+
+    output = []
+    for row in rows:
+        output.append(checks.state_check(row))
+
+    test_output = [
+        'state must be empty for category codes 3-6',
+        'Valid',
+        'state must be UT',
+        'state required for category codes 1, 2, or 7',
+        'Valid',
+    ]
+
+    assert output == test_output
+
+
+def test_zip_check():
+    # yapf: disable
+    rows = [
+        {'address_primary': '', 'category_code': 7},
+        {'address_primary': '', 'category_code': 2, 'bsl_lacks_address_flag': 0},
+        {'address_primary': '', 'category_code': 2, 'bsl_lacks_address_flag': 1},
+        {'address_primary': '', 'category_code': 3},
+        {'address_primary': 'addr', 'category_code': 3},
+        {'address_primary': 'addr', 'category_code': 2, 'bsl_lacks_address_flag': 1},
+        {'address_primary': 'addr', 'category_code': 2, 'bsl_lacks_address_flag': 0},
+        {'address_primary': 'addr', 'category_code': 7},
+    ]
+    # yapf: enable
+
+    output = []
+    for row in rows:
+        output.append(checks.address_primary_check(row))
+
+    test_output = [
+        'address_primary required for category_code 7',
+        'address_primary required for category codes 1 or 2 with bsl_lacks_address_flag set to 0',
+        'Valid',
+        'Valid',
+        'address_primary must be empty for category_codes 3-6',
+        'address_primary must be empty for category codes 1 or 2 with bsl_lacks_address_flag set to 1',
+        'Valid',
+        'Valid',
+    ]
+
+    assert output == test_output
+
+
+def test_zip_code_suffix_check():
+    # yapf: disable
+    rows = [
+        {'zip_code_suffix': '1234', 'category_code': 3},
+        {'zip_code_suffix': '1234', 'category_code': 1},
+        {'zip_code_suffix': '', 'category_code': 1},
+        {'zip_code_suffix': '', 'category_code': 3},
+        {'zip_code_suffix': 'abc', 'category_code': 1},
+        {'zip_code_suffix': '12345', 'category_code': 1},
+    ]
+    # yapf: enable
+
+    output = []
+    for row in rows:
+        output.append(checks.zip_code_suffix_check(row))
+
+    test_output = [
+        'zip_code_suffix must be empty for category_codes 3-6',
+        'Valid',
+        'zip_code_suffix must be a four-digit number',
+        'Valid',
+        'zip_code_suffix must be a four-digit number',
+        'zip_code_suffix must be a four-digit number',
+    ]
+
+    assert output == test_output
+
+
+def test_unit_count_check():
+    # yapf: disable
+    rows = [
+        {'unit_count': '', 'category_code': 1},
+        {'unit_count': '4', 'category_code': 1},
+        {'unit_count': '', 'category_code': 2},
+        {'unit_count': '4', 'category_code': 2},
+    ]
+    # yapf: enable
+
+    output = []
+    for row in rows:
+        output.append(checks.unit_count_check(row))
+
+    test_output = [
+        'unit_count required for category codes 1 and 3',
+        'Valid',
+        'Valid',
+        'unit_count must be empty for category codes 2, 4, 5, 6, 7',
+    ]
+
+    assert output == test_output
+
+
+def test_building_type_code_check():
+    # yapf: disable
+    rows = [
+        {'building_type_code': 'B', 'category_code': 2},
+        {'building_type_code': 'B', 'category_code': 1},
+        {'building_type_code': 'Y', 'category_code': 2},
+        {'building_type_code': 'Y', 'category_code': 1},
+    ]
+    # yapf: enable
+
+    output = []
+    for row in rows:
+        output.append(checks.building_type_code_check(row))
+
+    test_output = [
+        'building_type_code must be empty for category codes 2, 3, 5, 6, 7',
+        'Valid',
+        'building_type_code must be empty for category codes 2, 3, 5, 6, 7',
+        'building_type_code must be B, R, X, G, C, or E for category codes 1 and 4',
+    ]
+
+    assert output == test_output
+
+
+def test_non_bsl_code_check():
+    # yapf: disable
+    rows = [
+        {'non_bsl_code': 'S', 'category_code': 1},
+        {'non_bsl_code': 'S', 'category_code': 6},
+        {'non_bsl_code': 'Y', 'category_code': 1},
+        {'non_bsl_code': 'Y', 'category_code': 6},
+    ]
+    # yapf: enable
+
+    output = []
+    for row in rows:
+        output.append(checks.non_bsl_code_check(row))
+
+    test_output = [
+        'non_bsl_code must be empty for category codes 1, 2, 3, 4, 5, 7',
+        'Valid',
+        'non_bsl_code must be empty for category codes 1, 2, 3, 4, 5, 7',
+        'non_bsl_code must be S, L, G, H, F, E, P, N, or R for category code 6',
+    ]
+
+    assert output == test_output
+
+
+def test_bsl_lacks_address_flag_check():
+    # yapf: disable
+    rows = [
+        {'bsl_lacks_address_flag': '1', 'category_code': 1},
+        {'bsl_lacks_address_flag': '10', 'category_code': 1},
+        {'bsl_lacks_address_flag': '', 'category_code': 1},
+        {'bsl_lacks_address_flag': '1', 'category_code': 3},
+        {'bsl_lacks_address_flag': '', 'category_code': 3},
+    ]
+    # yapf: enable
+
+    output = []
+    for row in rows:
+        output.append(checks.bsl_lacks_address_flag_check(row))
+
+    test_output = [
+        'Valid',
+        'bsl_lacks_address_flag must have a value of 0 or 1 for category code 1 or 2',
+        'bsl_lacks_address_flag must have a value of 0 or 1 for category code 1 or 2',
+        'bsl_lacks_address_flag must be empty for category codes 3-7',
+        'Valid',
+    ]
+
+    assert output == test_output
+
+
+def test_latitude_check():
+    # yapf: disable
+    rows = [
+        {'latitude': '30.000000', 'category_code': 1},
+        {'latitude': '40.000000', 'category_code': 1},
+        {'latitude': '50.000000', 'category_code': 1},
+        {'latitude': '40.000000', 'category_code': 2},
+        {'latitude': '', 'category_code': 2},
+        {'latitude': '40.0000', 'category_code': 1},
+        {'latitude': '40.000000000', 'category_code': 1},
+    ]
+    # yapf: enable
+
+    output = []
+    for row in rows:
+        output.append(checks.latitude_check(row))
+
+    test_output = [
+        'latitude out of range 36.8 to 42.2',
+        'Valid',
+        'latitude out of range 36.8 to 42.2',
+        'latitude must be empty for category codes 2, 3, 4, 6, 7',
+        'Valid',
+        'latitude must have a minimum precision of 6 decimal places',
+        'Valid',
+    ]
+
+    assert output == test_output
+
+
+def test_longitude_check():
+    # yapf: disable
+    rows = [
+        {'longitude': '-120.000000', 'category_code': 1},
+        {'longitude': '-110.000000', 'category_code': 1},
+        {'longitude': '-100.000000', 'category_code': 1},
+        {'longitude': '-110.000000', 'category_code': 2},
+        {'longitude': '', 'category_code': 2},
+        {'longitude': '-110.0000', 'category_code': 1},
+        {'longitude': '-110.000000000', 'category_code': 1},
+    ]
+    # yapf: enable
+
+    output = []
+    for row in rows:
+        output.append(checks.longitude_check(row))
+
+    test_output = [
+        'longitude out of range -114.3 to -109',
+        'Valid',
+        'longitude out of range -114.3 to -109',
+        'longitude must be empty for category codes 2, 3, 4, 6, 7',
+        'Valid',
+        'longitude must have a minimum precision of 6 decimal places',
+        'Valid',
+    ]
+
+    assert output == test_output
+
+
+def test_column_name_check_all_good_names():
+    test_df = pd.DataFrame(
+        columns=[
+            'contact_name',
+            'contact_email',
+            'contact_phone',
+            'category_code',
+            'location_id',
+            'address_primary',
+            'city',
+            'state',
+            'zip_code',
+            'zip_code_suffix',
+            'unit_count',
+            'building_type_code',
+            'non_bsl_code',
+            'bsl_lacks_address_flag',
+            'latitude',
+            'longitude',
+        ]
+    )
+
+    matching, missing, extra = checks.column_name_check(test_df)
+
+    assert matching == {
+        'contact_name',
+        'contact_email',
+        'contact_phone',
+        'category_code',
+        'location_id',
+        'address_primary',
+        'city',
+        'state',
+        'zip_code',
+        'zip_code_suffix',
+        'unit_count',
+        'building_type_code',
+        'non_bsl_code',
+        'bsl_lacks_address_flag',
+        'latitude',
+        'longitude',
+    }
+    assert not missing
+    assert not extra
+
+
+def test_column_name_check_missing_and_extra():
+    test_df = pd.DataFrame(
+        columns=[
+            'contact_name',
+            'contact_email',
+            'contact_phone',
+            'category_code',
+            'location_id',
+            'address_primary',
+            'city',
+            'state',
+            'zip_code',
+            'zip_code_suffix',
+            'unit_count',
+            'building_type_code',
+            'non_bsl_code',
+            'bsl_lacks_address_flag',
+            'latitude',
+            'extralongitude',
+        ]
+    )
+
+    matching, missing, extra = checks.column_name_check(test_df)
+
+    assert matching == {
+        'contact_name',
+        'contact_email',
+        'contact_phone',
+        'category_code',
+        'location_id',
+        'address_primary',
+        'city',
+        'state',
+        'zip_code',
+        'zip_code_suffix',
+        'unit_count',
+        'building_type_code',
+        'non_bsl_code',
+        'bsl_lacks_address_flag',
+        'latitude',
+    }
+    assert missing == {'longitude'}
+    assert extra == {'extralongitude'}
